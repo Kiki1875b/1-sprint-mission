1ac42f0b4196af9d0ab0b4d84af2bb7acb627df6
Kiki1875b <kevinheo0413@gmail.com>
2025-05-19 14:04:58 +0900

    feat: added DiscodeitUserDetails, DiscodeitUserDetailsService

---
+++ src/main/java/com/sprint/mission/discodeit/security/auth/DiscodeitUserDetails.java
@@ -0,0 +1,30 @@
+package com.sprint.mission.discodeit.security.auth;
+
+import com.sprint.mission.discodeit.entity.User;
+import java.util.Collection;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RequiredArgsConstructor
+public class DiscodeitUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
